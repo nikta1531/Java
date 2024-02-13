c7f35e59f2f3c047100d31d5eaf229a8a14d9fad
Nikita <skorpion94@mail.ru>
2024-02-13 20:40:06 +0300

    Initial commit

---
+++ src/main/java/org/example/HomeWork/Telefon_Book/Main.java
@@ -0,0 +1,9 @@
+package org.example.HomeWork.Telefon_Book;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        new Commands().init();
+    }
+}
